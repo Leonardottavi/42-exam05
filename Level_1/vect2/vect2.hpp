20d95ae00e5e3966ad1eb61fa192ea4f8a1f889c
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-09-19 18:23:52 +0200

    Refactor vect2 class: simplify arithmetic and assignment operator implementations, remove redundant comments, and enhance code readability

--- Level_1/vect2/vect2.hpp
+++ Level_1/vect2/vect2.hpp
@@ -1,59 +1,37 @@
 #ifndef VECT2_HPP
 #define VECT2_HPP
-
 #include <iostream>
 
 class vect2 {
-private:
     int _data[2];
-
 public:
-    // Constructors
-    vect2() { _data[0] = 0; _data[1] = 0; }
-    vect2(int v1, int v2) { _data[0] = v1; _data[1] = v2; }
-    vect2(const vect2& original) { _data[0] = original._data[0]; _data[1] = original._data[1]; }
-    ~vect2() {}
+    vect2() { _data[0] = _data[1] = 0; }
+    vect2(int x, int y) { _data[0] = x; _data[1] = y; }
+    vect2(const vect2& o) { _data[0] = o._data[0]; _data[1] = o._data[1]; }
 
-    // Assignment operator
-    vect2& operator=(const vect2& other) {
-        if (this != &other) {
-            _data[0] = other._data[0];
-            _data[1] = other._data[1];
-        }
-        return *this;
-    }
+    vect2& operator=(const vect2& o) { _data[0] = o._data[0]; _data[1] = o._data[1]; return *this; }
 
-    // Accessor operators
-    int& operator[](int index) { return _data[index]; }
-    const int& operator[](int index) const { return _data[index]; }
+    int& operator[](int i) { return _data[i]; }
+    const int& operator[](int i) const { return _data[i]; }
 
-    // Arithmetic operators
-    vect2 operator+(const vect2& other) const;
-    vect2 operator-(const vect2& other) const;
-    vect2 operator*(int scalar) const;
+    vect2 operator+(const vect2& o) const { return vect2(_data[0] + o._data[0], _data[1] + o._data[1]); }
+    vect2 operator-(const vect2& o) const { return vect2(_data[0] - o._data[0], _data[1] - o._data[1]); }
+    vect2 operator*(int s) const { return vect2(_data[0] * s, _data[1] * s); }
 
-    // Increment/Decrement operators
+    vect2& operator+=(const vect2& o) { _data[0] += o._data[0]; _data[1] += o._data[1]; return *this; }
+    vect2& operator-=(const vect2& o) { _data[0] -= o._data[0]; _data[1] -= o._data[1]; return *this; }
+
     vect2& operator++() { ++_data[0]; ++_data[1]; return *this; }
-    vect2 operator++(int);
     vect2& operator--() { --_data[0]; --_data[1]; return *this; }
-    vect2 operator--(int);
+    vect2 operator++(int) { vect2 t(*this); ++(*this); return t; }
+    vect2 operator--(int) { vect2 t(*this); --(*this); return t; }
 
-    // Compound assignment operators
-    vect2& operator+=(const vect2& other);
-    vect2& operator-=(const vect2& other);
+    bool operator==(const vect2& o) const { return _data[0] == o._data[0] && _data[1] == o._data[1]; }
+    bool operator!=(const vect2& o) const { return !(*this == o); }
 
-    // Comparison operators
-    bool operator==(const vect2& other) const {
-        return _data[0] == other._data[0] && _data[1] == other._data[1];
-    }
-    bool operator!=(const vect2& other) const { return !(*this == other); }
-
-    // Friend function for scalar multiplication
-    friend vect2 operator*(int scalar, const vect2& vector) { return vector * scalar; }
-    friend std::ostream& operator<<(std::ostream& out, const vect2& v) {
-        out << "{" << v._data[0] << ", " << v._data[1] << "}";
-        return out;
+    friend vect2 operator*(int s, const vect2& v) { return v * s; }
+    friend std::ostream& operator<<(std::ostream& os, const vect2& v) {
+        return os << "{" << v._data[0] << ", " << v._data[1] << "}";
     }
 };
-
-#endif // VECT2_HPP
+#endif
