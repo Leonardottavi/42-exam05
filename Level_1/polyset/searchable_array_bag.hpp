5f3e9806872c9f01b750612d8aa460d60d0b83f7
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-09-19 15:57:13 +0200

    Refactor searchable_array_bag and searchable_tree_bag: remove redundant has method implementations and simplify set class methods

--- Level_1/polyset/searchable_array_bag.hpp
+++ Level_1/polyset/searchable_array_bag.hpp
@@ -4,19 +4,8 @@
 
 class searchable_array_bag : public array_bag, public searchable_bag {
 public:
-    searchable_array_bag() : array_bag() {}
-    searchable_array_bag(const searchable_array_bag& other) : array_bag(other) {}
-    searchable_array_bag& operator=(const searchable_array_bag& other) {
-        if (this != &other) {
-            array_bag::operator=(other);
-        }
-        return *this;
+    bool has(int item) const {
+        for (int i = 0; i < size; i++) if (data[i] == item) return true;
+        return false;
     }
-    ~searchable_array_bag() = default;
-    
-    void insert(int item) override { array_bag::insert(item); }
-    void insert(int* items, int count) { array_bag::insert(items, count); }
-    void print() const override { array_bag::print(); }
-    void clear() override { array_bag::clear(); }
-    bool has(int item) const override;
-}; 
+};
--- Level_1/polyset/set.hpp
+++ Level_1/polyset/set.hpp
@@ -2,20 +2,14 @@
 #include "searchable_bag.hpp"
 
 class set {
-private:
     searchable_bag& bag;
-
 public:
-    set(searchable_bag& b);
-    set(const set& other);
-    set& operator=(const set& other);
-    ~set();
-    
-    void insert(int item);
-    void insert(int* items, int count);
+    set(searchable_bag& b) : bag(b) {}
+    void insert(int item) { if (!bag.has(item)) bag.insert(item); }
+    void insert(int* items, int count) { for (int i = 0; i < count; i++) insert(items[i]); }
     bool has(int item) const { return bag.has(item); }
     void print() const { bag.print(); }
     void clear() { bag.clear(); }
     searchable_bag& get_bag() { return bag; }
     const searchable_bag& get_bag() const { return bag; }
-}; 
+};
