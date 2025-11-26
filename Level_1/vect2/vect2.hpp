1848869299c46e76e8bc3177138169a80800d2f0
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-11-26 22:03:03 +0100

    Refactor vect2 constructor and operator overloads: improve parameter naming for clarity and enhance consistency in operator implementations

--- Level_1/vect2/vect2.hpp
+++ Level_1/vect2/vect2.hpp
@@ -6,7 +6,7 @@ class vect2 {
 	int x, y;
 public:
 	vect2():x(0),y(0){}
-	vect2(int a,int b):x(a),y(b){}
+	vect2(int x,int y):x(x),y(y){}
 	vect2(const vect2& other):x(other.x),y(other.y){}
 	vect2& operator=(const vect2& other){x=other.x;y=other.y;return *this;}
 	~vect2(){}
@@ -14,16 +14,16 @@ public:
 	const int& operator[](int i)const{return i?y:x;}
 	vect2 operator+(const vect2& other)const{return vect2(x+other.x,y+other.y);}
 	vect2 operator-(const vect2& other)const{return vect2(x-other.x,y-other.y);}
-	vect2 operator*(int s)const{return vect2(x*s,y*s);}
+	vect2 operator*(int other)const{return vect2(x*other,y*other);}
 	vect2& operator+=(const vect2& other){x+=other.x;y+=other.y;return *this;}
 	vect2& operator-=(const vect2& other){x-=other.x;y-=other.y;return *this;}
 	vect2& operator++(){++x;++y;return *this;}
 	vect2& operator--(){--x;--y;return *this;}
-	vect2 operator++(int){vect2 t(*this);++*this;return t;}
-	vect2 operator--(int){vect2 t(*this);--*this;return t;}
+	vect2 operator++(int){vect2 other(*this);++*this;return other;}
+	vect2 operator--(int){vect2 other(*this);--*this;return other;}
 	bool operator==(const vect2& other)const{return x==other.x&&y==other.y;}
 	bool operator!=(const vect2& other)const{return !(*this==other);}
-	friend vect2 operator*(int s,const vect2& v){return v*s;}
-	friend std::ostream& operator<<(std::ostream& os,const vect2& v){return os<<"{"<<v.x<<", "<<v.y<<"}";}
+	friend vect2 operator*(int other,const vect2& other){return other*other;}
+	friend std::ostream& operator<<(std::ostream& other,const vect2& other){return other<<"{"<<other.x<<", "<<other.y<<"}";}
 };
 #endif
