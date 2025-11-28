e36866f48b6e422ad7e072796790c54a0f18afb0
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-11-28 14:15:32 +0100

    Refactor code structure for improved readability and maintainability

--- Level_1/polyset/searchable_tree_bag.hpp
+++ Level_1/polyset/searchable_tree_bag.hpp
@@ -15,12 +15,8 @@ public:
     ~searchable_tree_bag() {}
 
     bool has(int item) const {
-        node* current = tree;
-        while (current) {
-            if (item < current->value) current = current->l;
-            else if (item > current->value) current = current->r;
-            else return true;
-        }
+        for (node* current = tree; current; current = (item < current->value) ? current->l : current->r)
+            if (item == current->value) return true;
         return false;
     }
 };
