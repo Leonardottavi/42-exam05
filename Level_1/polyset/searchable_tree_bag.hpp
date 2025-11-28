6253d20fcb7dbc2057cb468a9d870e21eb885607
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-11-28 14:07:02 +0100

    Refactor: replace #pragma once with include guards and improve copy assignment operators in searchable_array_bag, searchable_tree_bag, and set classes

--- Level_1/polyset/searchable_tree_bag.hpp
+++ Level_1/polyset/searchable_tree_bag.hpp
@@ -1,9 +1,19 @@
-#pragma once
+#ifndef SEARCHABLE_TREE_BAG_HPP
+#define SEARCHABLE_TREE_BAG_HPP
+
 #include "provided/tree_bag.hpp"
 #include "provided/searchable_bag.hpp"
 
 class searchable_tree_bag : public tree_bag, public searchable_bag {
 public:
+    searchable_tree_bag() {}
+    searchable_tree_bag(const searchable_tree_bag& src) : tree_bag(src), searchable_bag(src) {}
+    searchable_tree_bag& operator=(const searchable_tree_bag& src) {
+        if (this != &src) tree_bag::operator=(src);
+        return *this;
+    }
+    ~searchable_tree_bag() {}
+
     bool has(int item) const {
         node* current = tree;
         while (current) {
@@ -14,3 +24,5 @@ public:
         return false;
     }
 };
+
+#endif
