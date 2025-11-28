01844a4d6726170392a8e011291b1a311530dc09
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-11-28 21:45:00 +0100

    1

--- Level_1/vect2/vect2.hpp
+++ Level_1/vect2/vect2.hpp
@@ -8,17 +8,17 @@ public:
 	vect2():x(0),y(0){}
 	vect2(int x,int y):x(x),y(y){}
 	vect2(const vect2& other):x(other.x),y(other.y){}
-	vect2& operator=(const vect2& other){x=other.x;y=other.y;return *this;}
 	~vect2(){}
+	vect2& operator=(const vect2& other){x=other.x;y=other.y;return *this;}
+	vect2& operator+=(const vect2& other){x+=other.x;y+=other.y;return *this;}
+	vect2& operator-=(const vect2& other){x-=other.x;y-=other.y;return *this;}
+	vect2& operator++(){++x;++y;return *this;}
+	vect2& operator--(){--x;--y;return *this;}
 	int& operator[](int i){return i?y:x;}
 	const int& operator[](int i)const{return i?y:x;}
 	vect2 operator+(const vect2& other)const{return vect2(x+other.x,y+other.y);}
 	vect2 operator-(const vect2& other)const{return vect2(x-other.x,y-other.y);}
 	vect2 operator*(int other)const{return vect2(x*other,y*other);}
-	vect2& operator+=(const vect2& other){x+=other.x;y+=other.y;return *this;}
-	vect2& operator-=(const vect2& other){x-=other.x;y-=other.y;return *this;}
-	vect2& operator++(){++x;++y;return *this;}
-	vect2& operator--(){--x;--y;return *this;}
 	vect2 operator++(int){vect2 other(*this);++*this;return other;}
 	vect2 operator--(int){vect2 other(*this);--*this;return other;}
 	bool operator==(const vect2& other)const{return x==other.x&&y==other.y;}
