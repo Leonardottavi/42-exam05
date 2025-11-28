0d7146760fd9f2675965572802398c236d445c69
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-11-28 22:03:01 +0100

    2

--- Level_1/vect2/vect2.hpp
+++ Level_1/vect2/vect2.hpp
@@ -18,11 +18,11 @@ public:
 	const int& operator[](int i)const{return i?y:x;}
 	vect2 operator+(const vect2& other)const{return vect2(x+other.x,y+other.y);}
 	vect2 operator-(const vect2& other)const{return vect2(x-other.x,y-other.y);}
+	bool operator==(const vect2& other)const{return x==other.x&&y==other.y;}
+	bool operator!=(const vect2& other)const{return !(*this==other);}
 	vect2 operator*(int other)const{return vect2(x*other,y*other);}
 	vect2 operator++(int){vect2 other(*this);++*this;return other;}
 	vect2 operator--(int){vect2 other(*this);--*this;return other;}
-	bool operator==(const vect2& other)const{return x==other.x&&y==other.y;}
-	bool operator!=(const vect2& other)const{return !(*this==other);}
 	friend vect2 operator*(int other,const vect2& other){return other*other;}
 	friend std::ostream& operator<<(std::ostream& other,const vect2& other){return other<<"{"<<other.x<<", "<<other.y<<"}";}
 };
