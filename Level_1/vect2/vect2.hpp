07a1f9a51518646e9d566aeaa53a16a945f97764
Lottavi <93330984+Leonardottavi@users.noreply.github.com>
2025-12-01 13:43:23 +0100

    1

--- Level_1/vect2/vect2.hpp
+++ Level_1/vect2/vect2.hpp
@@ -9,7 +9,7 @@ public:
 	vect2(int x,int y):x(x),y(y){}
 	vect2(const vect2& other):x(other.x),y(other.y){}
 	~vect2(){}
-	vect2& operator=(const vect2& other){x=other.x;y=other.y;return *this;}
+	vect2& operator=(const vect2& other){if(this!=&other){x=other.x;y=other.y;}return *this;}
 	vect2& operator+=(const vect2& other){x+=other.x;y+=other.y;return *this;}
 	vect2& operator-=(const vect2& other){x-=other.x;y-=other.y;return *this;}
 	vect2& operator++(){++x;++y;return *this;}
